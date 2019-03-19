0ce8b5c7c513a88d223d60b4a00e57ebb5709d25
lcdtyph <lcdtyph@gmail.com>
2019-03-20 02:22:51 +0800

    refine server start/stop

--- uvrestful/include/uvrestful/restful/server.h
+++ uvrestful/include/uvrestful/restful/server.h
@@ -20,11 +20,11 @@ typedef struct {
 
 typedef void (*uvrestful_server_handlers_drop_cb)(uvrestful_server_handlers *h);
 
-uvrestful_server *uvrestful_server_new(uv_loop_t *loop);
+uvrestful_server *uvrestful_server_new();
 void uvrestful_server_drop(uvrestful_server *s, uvrestful_server_handlers_drop_cb cb);
 void uvrestful_server_register(uvrestful_server *s, const char *uri, uvrestful_server_handlers *handlers);
 uvrestful_server_handlers *uvrestful_server_deregister(uvrestful_server *s, const char *uri);
-void uvrestful_server_start(uvrestful_server *s, uint16_t port);
+void uvrestful_server_start(uvrestful_server *s, uv_loop_t *loop, uint16_t port);
 void uvrestful_server_stop(uvrestful_server *s);
 
 #endif // __UVRESTFUL_RESTFUL_SERVER_H__
--- uvrestful/src/server.c
+++ uvrestful/src/server.c
@@ -10,24 +10,27 @@ struct uvrestful_server {
     uv_tcp_t *server;
     vstr_hash_map *handlers;
     int running;
+    int dropping;
 };
 
-uvrestful_server *uvrestful_server_new(uv_loop_t *loop) {
+uvrestful_server *uvrestful_server_new() {
     uvrestful_server *s = (uvrestful_server *)malloc(sizeof *s);
     s->server = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
     s->handlers = vstr_hash_map_new();
 
-    uv_tcp_init(loop, s->server);
     s->server->data = s;
     s->running = 0;
+    s->dropping = 0;
 
     return s;
 }
 
 static void __server_close_cb(uv_handle_t *handle) {
     uvrestful_server *s = (uvrestful_server *)handle->data;
-    free(handle);
-    free(s);
+    if (s->dropping) {
+        free(handle);
+        free(s);
+    }
 }
 
 static void __server_hash_walk_cb(vstr_hash_map *h, const char *k, void *v, void *arg) {
@@ -39,9 +42,15 @@ static void __server_hash_walk_cb(vstr_hash_map *h, const char *k, void *v, void
 }
 
 void uvrestful_server_drop(uvrestful_server *s, uvrestful_server_handlers_drop_cb cb) {
+    if (s->dropping) { return; }
+    s->dropping = 1;
     vstr_hash_map_walk(s->handlers, __server_hash_walk_cb, cb);
     vstr_hash_map_drop(s->handlers);
-    uv_close((uv_handle_t *)s->server, __server_close_cb);
+    if (s->running) {
+        uvrestful_server_stop(s);
+    } else {
+        __server_close_cb((uv_handle_t *)s->server);
+    }
 }
 
 void uvrestful_server_register(uvrestful_server *s, const char *uri, uvrestful_server_handlers *handlers) {
@@ -54,16 +63,20 @@ uvrestful_server_handlers *uvrestful_server_deregister(uvrestful_server *s, cons
 
 static void __server_on_conn(uv_stream_t* server, int status);
 
-void uvrestful_server_start(uvrestful_server *s, uint16_t port) {
+void uvrestful_server_start(uvrestful_server *s, uv_loop_t *loop, uint16_t port) {
     struct sockaddr_storage sock;
+    if (s->running) { return; }
     s->running = 1;
+    uv_tcp_init(loop, s->server);
     uv_ip6_addr("::", port, (struct sockaddr_in6 *)&sock);
     uv_tcp_bind(s->server, (struct sockaddr *)&sock, 0);
     uv_listen((uv_stream_t *)s->server, 2048, __server_on_conn);
 }
 
 void uvrestful_server_stop(uvrestful_server *s) {
+    if (!s->running) { return; }
     s->running = 0;
+    uv_close((uv_handle_t *)s->server, __server_close_cb);
 }
 
 static void __server_on_conn(uv_stream_t* server, int status) {
